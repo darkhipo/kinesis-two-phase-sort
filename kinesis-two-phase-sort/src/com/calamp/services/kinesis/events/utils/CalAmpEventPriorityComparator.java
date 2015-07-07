671fe93b3136275d5b55c658df036ea4b032ae1f
Dmitri Arkhipov <darkhipo@gmail.com>
2015-07-07 10:22:27 -0700

    Comparator is now tiered, ready for integration with CalAmp message classes.

--- kinesis-two-phase-sort/src/com/calamp/services/kinesis/events/utils/CalAmpEventPriorityComparator.java
+++ kinesis-two-phase-sort/src/com/calamp/services/kinesis/events/utils/CalAmpEventPriorityComparator.java
@@ -9,6 +9,23 @@ public class CalAmpEventPriorityComparator implements Comparator<com.calamp.serv
 	 */
 	@Override
 	public int compare(com.calamp.services.kinesis.events.utils.CalAmpEvent e1, com.calamp.services.kinesis.events.utils.CalAmpEvent e2){
-		return (int) ( e1.getMyTime() - e2.getMyTime() );
+		int c1 = (int) ( e1.getMyTime() - e2.getMyTime() );		//Sort by time-stamp 
+		int c2 = (int) ( e1.getId() - e2.getMyTime() );			//then by sequence number
+		int c3 = (int) ( e1.getPrice() - e2.getPrice() );		//then by boolean "is message_type==1" (true has precedence)
+		int c4 = (int) ( e1.getQuantity() - e2.getQuantity() );	//then by machine identifier.
+		
+		if( c1 != 0 ){
+			return c1;
+		}
+		if( c2 != 0 ){
+			return c2;
+		}
+		if( c3 != 0 ){
+			return c3;
+		}
+		if( c4 != 0 ){
+			return c4;
+		}
+		return 0;
 	}
 }
